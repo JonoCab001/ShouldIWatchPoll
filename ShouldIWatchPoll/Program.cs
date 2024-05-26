bc7cc881f69f6c94353e57f6caed14468b3e4a0f
Jono Cabrera <cjonathan193@gmail.com>
2024-05-26 21:06:57 +1000

    Add project files.

---
+++ ShouldIWatchPoll/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ShouldIWatchPoll
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            bool favActor = true;
+            bool favDrama = false;
+
+            bool shouldIWatchDecision = (favActor || favDrama);
+
+            if (shouldIWatchDecision)
+            {
+                Console.WriteLine("I'll definitely watch this one! :)");
+            }
+            else
+            {
+                Console.WriteLine("Nah, I'll skip it...");
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
